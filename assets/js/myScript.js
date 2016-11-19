8694bc80cc0d47f1521bdd77077729c23efadbc7
Gabriel Periotto <gabriel.periotto@my.westminster.ac.uk>
2016-11-19 20:40:04 +0000

    newsletter field validator

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,27 +1,45 @@
 var jQuery = jQuery.noConflict();
- 
+
 jQuery(document).ready(function ($) {
-    
+
     "use strict";
-    
+
     //Instantiate global popup
-    $( "#newsletterPopup" ).enhanceWithin().popup();
-    
+    $("#newsletterPopup").enhanceWithin().popup();
+
     $("#homePageCarousel").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
         singleItem: true
     });
-    
+
     //Fixes parrot img getting cut off
     $("#nav-panel").panel({
         beforeclose: function (event, ui) {
             $('[data-role="footer"]').css("overflow", "visible");
             $('.ui-footer').css("overflow", "visible");
-            $('.ui-header').css('overflow' , 'hidden');   
+            $('.ui-header').css('overflow', 'hidden');
         }
     });
-    
-//    $("input.newsletterButton").removeAttr('class');
+
+    //Newsletter field validator
+    var newsletterForm = document.getElementById('newsletterForm');
+    newsletterForm.onsubmit = function () {
+        if (newsletterForm.newsletterTextField.value == '') {
+            $("[data-role='popup'] [role='main'] .ui-corner-all:nth-child(1)").css("border-color", "#e26464");
+            $("[data-role='popup'] [role='main'] p.newsletterTextFieldValidation").text("Please enter your email above");
+            return false;
+        }
+    };
+ 
+    //Newsletter styling reset
+    $("#newsletterPopup").popup({
+        afteropen: function (event, ui) {
+            $("[data-role='popup'] [role='main'] .ui-corner-all:nth-child(1)").css("border-color", "#646464");
+            $("[data-role='popup'] [role='main'] input:nth-child(1)").val('');
+            $("[data-role='popup'] [role='main'] p.newsletterTextFieldValidation").text(" ");
+        }
+    });
+
 });
