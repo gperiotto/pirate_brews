a68a879ffb69e244483e91c74c45ca935655ce1a
Gabriel Periotto <gabriel.periotto@my.westminster.ac.uk>
2016-11-19 05:22:19 +0000

    added newsletter popup + fixed layout issues

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -4,6 +4,9 @@ jQuery(document).ready(function ($) {
     
     "use strict";
     
+    //Instantiate global popup
+    $( "#newsletterPopup" ).enhanceWithin().popup();
+    
     $("#homePageCarousel").owlCarousel({
         navigation : false, // Show next and prev buttons
         slideSpeed : 300,
@@ -11,4 +14,14 @@ jQuery(document).ready(function ($) {
         singleItem: true
     });
     
+    //Fixes parrot img getting cut off
+    $("#nav-panel").panel({
+        beforeclose: function (event, ui) {
+            $('[data-role="footer"]').css("overflow", "visible");
+            $('.ui-footer').css("overflow", "visible");
+            $('.ui-header').css('overflow' , 'hidden');   
+        }
+    });
+    
+//    $("input.newsletterButton").removeAttr('class');
 });
