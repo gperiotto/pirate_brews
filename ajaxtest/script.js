7edc66a34131d4508435d17fd8c0983bff8ecf94
SuperSassyCassy <w1524720@my.westminster.ac.uk>
2017-01-01 16:00:03 +0000

    ajaxtest

---
+++ ajaxtest/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+$("#submit").click(function(){
+var name = $("#name").val();
+var email = $("#email").val();
+var password = $("#password").val();
+var contact = $("#contact").val();
+// Returns successful data submission message when the entered information is stored in database.
+var dataString = 'name1='+ name + '&email1='+ email + '&password1='+ password + '&contact1='+ contact;
+if(name==''||email==''||password==''||contact=='')
+{
+alert("Please Fill All Fields");
+}
+else
+{
+// AJAX Code To Submit Form.
+$.ajax({
+type: "POST",
+url: "ajaxsubmit.php",
+data: dataString,
+cache: false,
+success: function(result){
+alert(result);
+}
+});
+}
+return false;
+});
+});
