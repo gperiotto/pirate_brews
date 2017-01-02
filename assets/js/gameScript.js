ab98b33f1c4f3bdc032611af32fde6d200ae677c
Gabriel Periotto <gabriel.periotto@my.westminster.ac.uk>
2017-01-02 00:03:51 +0000

    jquery plugin fix

---
+++ assets/js/gameScript.js
@@ -0,0 +1,22 @@
+var gamejQuery = jQuery.noConflict();
+
+gamejQuery(document).ready(function ($) {
+
+    "use strict";
+
+    //Instantiate global newsletterPopup
+    $("#newsletterPopup").enhanceWithin().popup();
+
+    //Instantiate global menuPanel
+    $("#navPanel").panel().enhanceWithin();
+
+
+
+
+    ////////////////////////////////////////////////////
+
+
+
+
+
+});
