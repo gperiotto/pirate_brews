67f8f54ae022898407b5f37b87966802ec61aff8
Gabriel Periotto <gabriel.periotto@my.westminster.ac.uk>
2017-01-02 15:06:37 +0000

    added gameSignIn page

--- assets/js/gameScript.js
+++ assets/js/gameScript.js
@@ -5,7 +5,7 @@ gamejQuery(document).ready(function ($) {
     "use strict";
 
     //Instantiate global newsletterPopup
-    $("#newsletterPopup").enhanceWithin().popup();
+    $("#gameRulesPopup").enhanceWithin().popup();
 
     //Instantiate GAME menuPanel
     $("#gameNavPanel").panel().enhanceWithin();
