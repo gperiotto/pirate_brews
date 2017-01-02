3664391ac0884c5d89e6dd5a85de28d0727dc6d8
Gabriel Periotto <gabriel.periotto@my.westminster.ac.uk>
2017-01-02 18:07:06 +0000

    added basic game page

--- assets/js/gameScript.js
+++ assets/js/gameScript.js
@@ -2,21 +2,44 @@ var gamejQuery = jQuery.noConflict();
 
 gamejQuery(document).ready(function ($) {
 
-    "use strict";
+	"use strict";
 
-    //Instantiate global newsletterPopup
-    $("#gameRulesPopup").enhanceWithin().popup();
+	//Instantiate global newsletterPopup
+	$("#gameRulesPopup").enhanceWithin().popup();
 
-    //Instantiate GAME menuPanel
-    $("#gameNavPanel").panel().enhanceWithin();
+	$("#quitGamePopup").enhanceWithin().popup();
 
+	$("#gameFavPopup").enhanceWithin().popup();
 
-
-
-    ////////////////////////////////////////////////////
-
+	//Instantiate GAME menuPanel
+	$("#gameNavPanel").panel().enhanceWithin();
 
 
 
+
+	////////////////////////////////////////////////////
+	//Game pave slide animation 
+	$(document).on('pagebeforeshow', '#signIn_page', function () {
+		$(document).on('click', '.gameButton', function () {
+			$.mobile.navigate("#game_page", {
+				transition: "slide"
+			});
+		});
+	});
+	////////////////////////////////////////////////////
+
+	$("#gameFavPopup").popup({
+		afteropen: function (event, ui) {
+			$("#game_page [data-role=footer] .footerTaskbar1 p").css("color", "#f03820");
+			
+//			$("#game_page [data-role=footer] .footerTaskbar1 p").text(" ");
+		}
+	});
+
+
+
+
+	////////////////////////////////////////////////////
+	//END OF SCRIPT
 
 });
