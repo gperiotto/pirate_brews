622348164e2a70c70daf63d3aef077528475239e
Gabriel Periotto <gabriel.periotto@my.westminster.ac.uk>
2017-01-04 22:20:13 +0000

    Added pub boundaries and user location

--- assets/js/gameScript.js
+++ assets/js/gameScript.js
@@ -53,10 +53,18 @@ gamejQuery(document).ready(function ($) {
 	////////////////////////////////////////////////////
 	
 	//Map GLOBAL VARIABLES
-	var map;
-	var cav_Marker, mar_Marker, tit_Marker, reg_Marker;
-	var cav_InfoContent, mar_InfoContent, tit_InfoContent, reg_InfoContent;
-	var cav_InfoWindow, mar_InfoWindow, tit_InfoWindow, reg_InfoWindow;
+	
+	var map;//GOOGLE MAPS OBJ
+	var cav_Marker, mar_Marker, tit_Marker, reg_Marker;//PUB MARKERS	
+	var cav_InfoContent, mar_InfoContent, tit_InfoContent, reg_InfoContent;//PUB INFO CONTENT	
+	var cav_InfoWindow, mar_InfoWindow, tit_InfoWindow, reg_InfoWindow;//PUB INFO WINDOWS
+	var userCurrentPos;//USER CURRENT COORD
+	var infoWindow;//USER CURRENT COORD INFO WINDOW
+	var userPosMarker;//MARKER USED TO SHOW USER POSITION
+	var posBoolean=0;//variable used to update user's position. Don't judge
+	var cav_latlng, mar_latlng, tit_latlng, reg_latlng;//Pubs coords - google.maps.LatLng obj
+	var cav_Circle, mar_Circle, tit_Circle, reg_Circle;//PUB boundaries circle
+	var cav_pubCircle, mar_pubCircle, tit_pubCircle, reg_pubCircle;
 	
 	$(document).on('pageshow', '#game_page', function (e, data) {
 		setTimeout(function () {
@@ -72,10 +80,7 @@ gamejQuery(document).ready(function ($) {
 			styles: [{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"on"},{"saturation":"-100"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#dddddd"},{"lightness":40},{"visibility":"on"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#7f8d89"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2b3638"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2b3638"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.icon","stylers":[{"visibility":"off"}]}]
 		};
 			
-			
-			
-			
-			
+
 			//MAP OBJECT
 			var mapElement = document.getElementById('map');
 			map = new google.maps.Map(mapElement, mapOptions);
@@ -127,7 +132,7 @@ gamejQuery(document).ready(function ($) {
 				content: reg_InfoContent
 			});
 			
-			
+			infoWindow = new google.maps.InfoWindow({map: map});
 			
 			//////////////////////////////////////////////////
 			//MARKER CLICK LISTENERS
@@ -146,11 +151,144 @@ gamejQuery(document).ready(function ($) {
 			reg_Marker.addListener('click', function() {
 			  	reg_InfoWindow.open(map, reg_Marker);
 			});
+			///////////////////////////////////////////////////
 			
+			
+			cav_Marker.setIcon('assets/ico/pub_marker_logo.png');
+			mar_Marker.setIcon('assets/ico/pub_marker_logo.png');
+			tit_Marker.setIcon('assets/ico/pub_marker_logo.png');
+			reg_Marker.setIcon('assets/ico/pub_marker_logo.png');
 
+			//USER CURRENT LOCATION
+      		findMe();
+			
+      		addPubRadius();
+			
 			
 		}, 1);
+	}); 
+	
+	function addPubRadius(){
+		cav_latlng = new google.maps.LatLng(51.520780, -0.139900);
+		mar_latlng = new google.maps.LatLng(51.522351, -0.154974);
+		tit_latlng = new google.maps.LatLng(51.518154, -0.141053);
+		reg_latlng = new google.maps.LatLng(51.516931, -0.142847);
+		
+		
+		cav_Circle = {
+			strokeColor: "#ffffff",
+			strokeOpacity: 0.8,
+			strokeWeight: 2,
+			fillColor: "#dedede",
+			fillOpacity: 0.35,
+			map: map,
+			center: cav_latlng,
+			radius: 60 // in meters
+		};	
+		mar_Circle = {
+			strokeColor: "#ffffff",
+			strokeOpacity: 0.8,
+			strokeWeight: 2,
+			fillColor: "#dedede",
+			fillOpacity: 0.35,
+			map: map,
+			center: mar_latlng,
+			radius: 60 // in meters
+		};	
+		tit_Circle = {
+			strokeColor: "#ffffff",
+			strokeOpacity: 0.8,
+			strokeWeight: 2,
+			fillColor: "#dedede",
+			fillOpacity: 0.35,
+			map: map,
+			center: tit_latlng,
+			radius: 60 // in meters
+		};
+		reg_Circle = {
+			strokeColor: "#ffffff",
+			strokeOpacity: 0.8,
+			strokeWeight: 2,
+			fillColor: "#dedede",
+			fillOpacity: 0.35,
+			map: map,
+			center: reg_latlng,
+			radius: 60 // in meters
+		};
+		
+		cav_pubCircle = new google.maps.Circle(cav_Circle);
+		cav_pubCircle.bindTo('center', cav_Marker, 'position');
+		
+		mar_pubCircle = new google.maps.Circle(mar_Circle);
+		mar_pubCircle.bindTo('center', mar_Marker, 'position');
+		
+		tit_pubCircle = new google.maps.Circle(tit_Circle);
+		tit_pubCircle.bindTo('center', tit_Marker, 'position');
+		
+		reg_pubCircle = new google.maps.Circle(reg_Circle);
+		reg_pubCircle.bindTo('center', reg_Marker, 'position');
+	}
+	
+	//FUNCTION FOR FINDING USER LOCATION
+	function findMe(){
+		// Try HTML5 geolocation.
+		if (navigator.geolocation) {
+		  navigator.geolocation.getCurrentPosition(function(position) {
+			userCurrentPos = {
+			  lat: position.coords.latitude,
+			  lng: position.coords.longitude
+			};
+
+			infoWindow.setPosition(userCurrentPos);
+			infoWindow.setContent('You are here!');
+			map.setCenter(userCurrentPos);
+		  }, function() {
+			handleLocationError(true, infoWindow, map.getCenter());
+		  });
+		} else {
+		  // Browser doesn't support Geolocation
+		  handleLocationError(false, infoWindow, map.getCenter());
+		}
+	}
+
+	//GEOLOCATION FAILED HANDLER
+	function handleLocationError(browserHasGeolocation, infoWindow, userCurrentPos) {
+        infoWindow.setPosition(userCurrentPos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
+	
+	//GAME GPS BUTTON click LISTNER
+	$("#gameUserLocationBtn").on('click', function(){
+		findMe();
+		addMarker();
 	});
+	
+	//ADDS MARKER AND UPDATES LOCATION
+	function addMarker(){
+
+		if(posBoolean===0){
+			userPosMarker = new google.maps.Marker({
+				position: new google.maps.LatLng(userCurrentPos.lat,userCurrentPos.lng),
+				map: map,
+				animation: google.maps.Animation.DROP,
+				title: 'Here you are!'
+			});
+
+			//ADDS MARKER TO MAP
+			userPosMarker.setMap(map);
+//			userPosMarker.setIcon('assets/ico/gps.png');
+			
+			//lazy way of implementing position update
+			posBoolean=posBoolean+1;
+		}else{
+			//updates current location
+			userPosMarker.setPosition(userCurrentPos);
+		}
+	}
+
+	
 	
 	cav_InfoContent = '<div id="InfoBoxContent">'+
             '<div id="siteNotice">'+
@@ -186,10 +324,6 @@ gamejQuery(document).ready(function ($) {
             '</div>';
 	
 	
-	function addMarker(){
-		//ADDS MARKER TO MAP
-//		marker2.setMap(map);
-	}
 
 		
 	
